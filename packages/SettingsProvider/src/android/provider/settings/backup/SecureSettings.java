PixelOS-Fourteen/frameworks_base

!fixup Hide power menu on secure lockscreen [1/2]
Change-Id: I07255a21a11e8a7ec3229b1d286b5d686b8d054a
Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -244,7 +244,6 @@ public class SecureSettings {
         Settings.Secure.SEARCH_PRESS_HOLD_NAV_HANDLE_ENABLED,
         Settings.Secure.SEARCH_LONG_PRESS_HOME_ENABLED,
         Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
-        Settings.Secure.DOZE_TAP_GESTURE_AMBIENT,
-        Settings.Secure.POWER_MENU_HIDE_ON_SECURE
+        Settings.Secure.DOZE_TAP_GESTURE_AMBIENT
     };
 }

EunoiaOS/android_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -242,7 +242,6 @@ public class SecureSettings {
         Settings.Secure.HEARING_AID_SYSTEM_SOUNDS_ROUTING,
         Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
         Settings.Secure.SEARCH_PRESS_HOLD_NAV_HANDLE_ENABLED,
-        Settings.Secure.SEARCH_LONG_PRESS_HOME_ENABLED,
-        Settings.Secure.POWER_MENU_HIDE_ON_SECURE
+        Settings.Secure.SEARCH_LONG_PRESS_HOME_ENABLED
     };
 }